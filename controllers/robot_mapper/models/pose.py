28a28e6113aecf05cf3fbb09ae9eebf27606f5fb
Dema <toropovdema@gmail.com>
2026-05-08 15:40:18 +0500

    feature: fix pose model

--- controllers/robot_mapper/models/pose.py
+++ controllers/robot_mapper/models/pose.py
@@ -7,10 +7,11 @@ class Pose:
     """Положение робота в 2D пространстве."""
     x: float
     y: float
-    theta: float  # радианы, 0 = смотрит на восток
+    theta: float  # радианы, 0 = восток, + = против часовой
 
     def distance_to(self, other: "Pose") -> float:
         return math.hypot(self.x - other.x, self.y - other.y)
 
-    def to_tuple(self) -> tuple[float, float]:
-        return self.x, self.y
+    def angle_to(self, other: "Pose") -> float:
+        """Угол от текущего положения к другому."""
+        return math.atan2(other.y - self.y, other.x - self.x)
